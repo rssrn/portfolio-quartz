dd18c84241ab819892718c867b40f11fc55df2b3
Ross Arnold <rossarn@gmail.com>
2026-01-26 13:11:32 +0000

    fix typo in last commit

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     /*enablePopovers: true,*/
     analytics: {
-      provider: "null",
+      provider: null,
     },
     head: {
       scripts: [
