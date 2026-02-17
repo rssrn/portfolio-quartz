8b4342e0464ccef71c0fa6527e55f84632ab20db
Ross Arnold <rossarn@gmail.com>
2026-02-17 14:19:31 +0000

    Change base URL in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     /*enablePopovers: true,*/
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "portfolio.rossarn.workers.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
